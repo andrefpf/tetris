e8782d26040db0b4da6f447bce3ea647545a49be
André Filipe da Silva Fernandes <fpf.andre@gmail.com>
2019-04-24 21:49:52 -0300

    i have tryed to use a grid

--- src/js/Piece.js
+++ src/js/Piece.js
@@ -1,14 +1,14 @@
 class Piece{
-    constructor(canvas){
-       this.py = 0
-       this.px = 50
-       this.vy = 1
-       this.size = 20
-       this.screenHeight = canvas.height
-       this.screenWidth = canvas.width
-       this.px = constrain(this.px, 0, (canvas.width - this.size))
-       console.log(this.px)
-       this.py = constrain(this.py, 0, (canvas.height - this.size))
+    constructor(canvas, posx){
+      this.size = 20
+      this.py = 0
+      this.px = canvas.width/2
+      this.vy = this.size
+      this.screenHeight = canvas.height
+      this.screenWidth = canvas.width
+      this.px = constrain(this.px, 0, (canvas.width - this.size))
+      this.py = constrain(this.py, 0, (canvas.height - this.size))
+      frameRate(7)
     }
 
     show(){
@@ -17,10 +17,22 @@ class Piece{
     }
 
     update(){
-        this.py = constrain(this.py + this.vy, 0, (canvas.height - this.size))
+
+        if(this.py > canvas.height - 2*this.size){
+          newPiece()
+        }
+        else if (piece.length > 1 && this.py > piece[0].py -2*this.size && this.px == piece[0].px){
+          newPiece()
+        }
+        else {
+          this.py += this.vy
+        }
+
+        //this.py = constrain(this.py + this.vy, 0, (canvas.height - this.size))
     }
 
     move(coord){
         this.px = constrain(this.px + (this.size * coord), 0, (canvas.width - this.size))
+        if(coord == 0)frameRate(15)
     }
 }
--- src/js/Tetris.js
+++ src/js/Tetris.js
@@ -1,16 +1,55 @@
-let piece 
+let scale = 60
+let squareNum = 5
+let grid = []
+
+let squares = []
+
+x = 3
+y = 1
 
 function setup(){
-    let canvas = createCanvas(400,400)
-    piece = new Piece(canvas)
+    let canvas = createCanvas(scale*squareNum, scale*squareNum)
     document.querySelector('main').appendChild(canvas.elt)
+
+    reset()
+    squares[x][y] = 1
 }
 
 function draw(){
     background(0)
-    piece.update()
-    piece.show()
+    
+    checkGrid()
+
 }
 
-window.addEventListener("keydown", ev=>{ if (ev.code == "ArrowRight") piece.move(1)})
-window.addEventListener("keydown", ev=>{ if (ev.code == "ArrowLeft") piece.move(-1)})
+function checkGrid(){
+    for (i in squares){
+      for(j=0; j<squareNum; j++){
+        if(squares[i][j] == 1){
+          console.log('a');
+          fill(255)
+          rect(20,10,20,20)
+        }
+
+      }
+    }
+}
+
+function reset(){
+    squares = []
+
+    for (i=0; i<squareNum; i++){
+      y = []
+
+      for(j=0; j<squareNum; j++){
+        y.push(0)
+      }
+
+      squares.push(y)
+    }
+}
+
+
+window.addEventListener("keydown", ev=>{ if (ev.code == "ArrowRight") piece[piece.length -1].move(1)})
+window.addEventListener("keydown", ev=>{ if (ev.code == "ArrowLeft") piece[piece.length -1].move(-1)})
+window.addEventListener("keydown", ev=>{ if (ev.code == "ArrowDown") piece[piece.length -1].move(0)})
