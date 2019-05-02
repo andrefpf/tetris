bc264dfce443593a13738f05cd4a8b467ae1c90a
André Filipe da Silva Fernandes <fpf.andre@gmail.com>
2019-05-02 06:08:35 -0300

    now the game is running. Just need to implement rotation

--- src/js/Piece.js
+++ src/js/Piece.js
@@ -7,24 +7,55 @@ class Piece{
         this.y = y
 
         this.falling = function(){
-            this.previousPosition()
-            this.y ++
-            this.checkCollision()
+            // this.previousPosition()
+            // this.checkCollision()
+            // this.y ++
+            move(0, 1)
+
         }
 
         this.checkCollision = function(){
-            if(this.y >= squareNum){
-                this.y = 0
+            if(this.y+1 >= squareNum){
+
+                squares[this.y][this.x] = 1
+                this.y = y
                 checkTetris()
             }
-            else if (squares[this.y][this.x] == 1){
-                this.y = 0
+            else if (squares[this.y+1][this.x] == 1){
+
+                squares[this.y][this.x] = 1
+                this.y = y
                 checkTetris()
                 // console.log('a');
             }
             else{
                 squares[this.yP][this.xP] = 0
+
             }
+        }
+
+        this.restart = function(){
+
+          // squares[this.y][this.xP] = 0
+
+          squares[this.y][this.x] = 1
+
+          this.x = x
+          this.y = y
+
+        }
+
+        this.collide = function(x,y){
+            if(this.y + y >= squareNum){return 2*y}
+
+            else if(this.x + x >= squareNum){return 1}
+            else if(this.x + x < 0){return 1}
+
+            else if(squares[this.y + y][this.x + x] == 1){return y*2 + x}
+
+            else{return 0}
+
+            if(squares[this.y + y][this.x] == 1){return 3}
         }
 
         this.previousPosition = function(){
--- src/js/Tetris.js
+++ src/js/Tetris.js
@@ -9,30 +9,24 @@ function setup(){
     let canvas = createCanvas(scale*squareNum, scale*squareNum)
     document.querySelector('main').appendChild(canvas.elt)
 
+    piece.push(new Piece(2,1))
+    piece.push(new Piece(3,1))
     piece.push(new Piece(3,0))
-    // piece.push(new Piece(4,0))
+    piece.push(new Piece(4,1))
 
     newBoard()
 
-    setInterval(function(){
-
-        for(p=0; p<piece.length; p++){
-
-          piece[0].falling()
-
-        }
-
-    }, 1000)
+    setInterval(function fallling() {move(0,1)}, 1000)
 }
 
 function draw(){
 
     for(p=0; p<piece.length; p++){
 
-        piece[p].x = constrain(piece[p].x, 0, squareNum-1)
-        piece[p].y = constrain(piece[p].y, 0, squareNum-1)
+        // piece[p].x = constrain(piece[p].x, 0, squareNum-1)
+        // piece[p].y = constrain(piece[p].y, 0, squareNum-1)
 
-        squares[piece[p].y][piece[p].x] = 1
+        squares[piece[p].y][piece[p].x] = 2
 
     }
 
@@ -43,7 +37,7 @@ function draw(){
 function checkGrid(){
     for (i in squares){
         for(j in squares[i]){
-            if(squares[i][j] == 1){
+            if(squares[i][j] != 0){
                 fill(255, 50, 100)
                 noStroke()
                 rect(scale*j,scale*i,scale,scale)
@@ -88,14 +82,53 @@ function tetris(line){
     for(i=line; i>0; i--){
         squares[i] = squares[i-1]
     }
-    squares[0] = [0,0,0,0,0]
+    squares[0] = [0]
 }
 
-window.addEventListener("keydown", ev=>{
+function move(mx, my){
+
+    possibleMove = true
+    restart = false
+
     for(p=0; p<piece.length; p++){
+        // console.log(piece[p].collide(mx,my));
+        if(piece[p].collide(mx, my) >= 2){restart = true}
+        if(piece[p].collide(mx, my) != 0){possibleMove = false}
         piece[p].previousPosition()
-        if (ev.code == "ArrowRight" && squares[piece[p].y][piece[0].x+1] == 0) {piece[p].x ++; piece[p].checkCollision()}
-        if (ev.code == "ArrowLeft"  && squares[piece[p].y][piece[0].x-1] == 0) {piece[p].x --; piece[p].checkCollision()}
-        if (ev.code == "ArrowDown") {piece[p].y ++; piece[p].checkCollision()}
+
     }
+
+    if(possibleMove){
+        for(p=0; p<piece.length; p++){
+            piece[p].x += mx
+            piece[p].y += my
+
+            squares[piece[p].yP][piece[p].xP] = 0
+        }
+    }
+
+    if(restart){
+        for(p=0; p<piece.length; p++){
+            piece[p].restart()
+        }
+        checkTetris()
+    }
+
+}
+
+// function rotate(){
+//
+//     for(p=0; p<piece.length; p++){
+//
+//         piece[p].x
+//
+//     }
+//
+// }
+
+window.addEventListener("keydown", ev=>{
+
+        if (ev.code == "ArrowRight") {move( 1, 0)}
+        if (ev.code == "ArrowLeft" ) {move(-1, 0)}
+        if (ev.code == "ArrowDown" ) {move( 0, 1)}
 })
